6fa8371b60b7e9e92e3d04d5b05d0cbed234bac0
shiv <shivchandekar2805@gmail.com>
2025-06-24 23:49:28 +0530

    initial commit

---
+++ src/main/java/com/striver/dsa/Striver/DSA/StriverDsaApplication.java
@@ -0,0 +1,13 @@
+package com.striver.dsa.Striver.DSA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StriverDsaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StriverDsaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/striver/dsa/Striver/DSA/StriverDsaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.striver.dsa.Striver.DSA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StriverDsaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
