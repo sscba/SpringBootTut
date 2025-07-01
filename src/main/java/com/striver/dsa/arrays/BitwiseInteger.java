7efa0f50c896282b9b09a657e68ca50ccbb7f695
shiv <shivchandekar2805@gmail.com>
2025-07-01 09:00:24 +0530

    longest subarray sum 0

---
+++ src/main/java/com/striver/dsa/arrays/BitwiseInteger.java
@@ -0,0 +1,9 @@
+package com.striver.dsa.arrays;
+
+public class BitwiseInteger {
+
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/striver/dsa/arrays/LongestSubarraySum0.java
@@ -0,0 +1,29 @@
+package com.striver.dsa.arrays;
+
+import java.util.HashMap;
+
+public class LongestSubarraySum0 {
+
+    public int solve(int[] nums){
+        int max = 0;
+        int n = nums.length;
+        int sum = 0;
+        HashMap<Integer,Integer> prefixSumMap = new HashMap<>();
+
+        for(int i=0;i<n;i++){
+            sum += nums[i];
+            if(prefixSumMap.containsKey(sum)){
+                max = Math.max(max, i- prefixSumMap.get(sum));
+            }else{
+                prefixSumMap.put(sum,i);
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {9,-3,3,-1,6,-5};
+        int subArraySum0= new LongestSubarraySum0().solve(nums);
+        System.out.println(subArraySum0);
+    }
+}
