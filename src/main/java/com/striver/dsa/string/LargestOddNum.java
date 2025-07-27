9627dd040c9b0830b25dfcf3d1bfad63cf0999eb
shiv <shivchandekar2805@gmail.com>
2025-07-27 17:36:15 +0530

    LargestOddNum

--- src/main/java/com/striver/dsa/string/LargestOddNum.java
+++ src/main/java/com/striver/dsa/string/LargestOddNum.java
@@ -1,4 +1,23 @@
 package com.striver.dsa.string;
 
 public class LargestOddNum {
+    public String largestOddNumber(String num){
+        int i = num.length() -1;
+
+        while(i>=0){
+            if(Integer.parseInt(String.valueOf(num.charAt(i))) %2  != 0){
+                return num.substring(0,i+1);
+            }
+            i--;
+        }
+        return "";
+    }
+
+    public static void main(String[] args) {
+        String nums = "3124242";
+
+        String ans = new LargestOddNum().largestOddNumber(nums);
+        System.out.println(ans);
+
+    }
 }
