fc911628cb27540e371e8c4b06490fccf0e7986a
shiv <shivchandekar2805@gmail.com>
2025-08-03 17:28:21 +0530

    DeleteNOde LinkedList

--- src/main/java/com/striver/dsa/linkedlist/DeleteNode.java
+++ src/main/java/com/striver/dsa/linkedlist/DeleteNode.java
@@ -1,4 +1,23 @@
 package com.striver.dsa.linkedlist;
 
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
+
 public class DeleteNode {
+
+    //    237. Delete Node in a Linked List - https://leetcode.com/problems/delete-node-in-a-linked-list/description/
+    public void deleteNode(ListNode node) {
+        ListNode temp = node.next;
+        node.val = temp.val;
+        node.next = temp.next;
+        temp = null;
+    }
+
+
 }
