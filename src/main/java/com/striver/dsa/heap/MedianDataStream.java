50ce8c5beb262779b02809015f70d489bd972a17
shivC <shivchandekar2805@gmail.com>
2025-11-03 23:48:59 +0530

    Median of data stream

--- src/main/java/com/striver/dsa/heap/MedianDataStream.java
+++ src/main/java/com/striver/dsa/heap/MedianDataStream.java
@@ -1,4 +1,35 @@
 package com.striver.dsa.heap;
 
+import java.util.PriorityQueue;
+import java.util.Queue;
+
 public class MedianDataStream {
+
+    private Queue<Integer> maxHeap;
+    private Queue<Integer> minHeap;
+    public MedianDataStream() {
+        maxHeap = new PriorityQueue<>((a,b) -> b-a);
+        minHeap = new PriorityQueue<>((a,b)-> a-b);
+    }
+
+    public void addNum(int num) {
+        if(maxHeap.isEmpty() || maxHeap.peek() >= num){
+            maxHeap.add(num);
+        }else{
+            minHeap.add(num);
+        }
+
+        if(maxHeap.size() > minHeap.size()+1){
+            minHeap.add(maxHeap.poll());
+        }else if(maxHeap.size() < minHeap.size()){
+            maxHeap.add(minHeap.poll());
+        }
+    }
+
+    public double findMedian() {
+        if(maxHeap.size() == minHeap.size()){
+            return maxHeap.peek()/2.0 + minHeap.peek()/2.0;
+        }
+        return maxHeap.peek()/1.0;
+    }
 }
